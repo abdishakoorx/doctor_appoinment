c9bd5b53f1ba4d5028229a1c02563b354179e12e
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-07-04 03:08:48 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
+    images: {
+        remotePatterns: [
             {
-                protocol:'https',
-                hostname:'ap-south-1.graphassets.com',
-                port:'',
-                pathname:'/**'
+                protocol: 'https',
+                hostname: 'ap-south-1.graphassets.com',
+                port: '',
+                pathname: '/**'
             },
             {
-                protocol:'https',
-                hostname:'lh3.googleusercontent.com',
-                port:'',
-                pathname:'/**'
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**'
             },
             {
-                protocol:'https',
-                hostname:'gravatar.com',
-                port:'',
-                pathname:'/**'
+                protocol: 'https',
+                hostname: 'gravatar.com',
+                port: '',
+                pathname: '/**'
             }
         ]
-    }
+    },
+    images: {
+        unoptimized: true
+    },
 };
 
 export default nextConfig;
